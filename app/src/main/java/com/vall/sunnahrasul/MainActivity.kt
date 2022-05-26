2b05f5435a1c64cc62faea75abeebc9ea6b4e544
NovalAdi <novaladiperasetya@gmail.com>
2022-05-26 21:01:18 +0700

    new

--- app/src/main/java/com/vall/sunnahrasul/MainActivity.kt
+++ app/src/main/java/com/vall/sunnahrasul/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnToPuasaSholat.setOnClickListener {
+        binding.btnPuasaSholat.setOnClickListener {
             startActivity(Intent(this, PuasaSholatActivity::class.java))
         }
 
